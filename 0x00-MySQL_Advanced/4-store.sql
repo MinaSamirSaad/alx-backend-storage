5455c0208c446a4c77485425124c6e6a35ffdbb4
MinaSamirSaad <minasamir.nashid@gmail.com>
2024-07-11 20:35:36 +0300

    Buy buy buy

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,12 @@
+-- creates a trigger that decreases the quantity of an item after adding a new order
+DROP TRIGGER IF EXISTS decreases_the_quantity;
+DELIMITER $$
+CREATE TRIGGER `decreases_the_quantity` 
+AFTER INSERT 
+ON order FOR EACH ROW
+BEGIN
+    UPDATE items
+        SET quantity = quantity - NEW.number
+        WHERE name = NEW.item_name;
+END $$
+DELIMITER ;
